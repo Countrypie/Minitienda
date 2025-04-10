76e155661e780897578279b6fbdba442dd0a9d61
Countrypie <62346749+Countrypie@users.noreply.github.com>
2025-04-10 16:59:33 +0200

    Version funcional 1

--- WEB-INF/classes/Seleccionar.java
+++ WEB-INF/classes/Seleccionar.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import Carrito.java;
 
 public class Seleccionar extends HttpServlet{
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         
         response.setContentType("text/html");
 
-        Carrito carrito= new Carrito(request.getParameter("titulo"),request.getParameter("cantidad"));
-        carrito.anadirCD();
+        Carrito carrito= new Carrito();
+        carrito.anadirCD(request.getParameter("titulo"),
+            Integer.parseInt(request.getParameter("cantidad")));
     }
 
 }
