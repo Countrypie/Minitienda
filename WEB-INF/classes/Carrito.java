ba559851bd4a0a4612be1cc93a53d674aedb2126
Countrypie <62346749+Countrypie@users.noreply.github.com>
2025-04-24 17:42:22 +0200

    Version 1
    Maneja carrito y funciona

--- WEB-INF/classes/Carrito.java
+++ WEB-INF/classes/Carrito.java
@@ -6,11 +6,12 @@ public class Carrito {
 
     private HashMap<String, Integer> cds;
 
+    //Constructor
     public Carrito(){
         this.cds= new HashMap<String, Integer>();
     }
 
-
+    //Añade un cd al carrito
     public void anadirCD(String titulo, int cantidad){
         
         if(this.cds.containsKey(titulo)){
@@ -20,11 +21,51 @@ public class Carrito {
         }else{
             this.cds.put(titulo, cantidad);
         }
-
-        System.out.printf("%s %d\n", titulo, this.cds.get(titulo));
     }
 
-    private void imptimirTodo(){
+    //Devuelve la estructura con todos los cds
+    public HashMap<String, Integer> getCds(){
+        return this.getCds();
+    }
+
+    //Devuelve el importe asociado a un cd
+    public float getImporte(String cd){
+
+        StringTokenizer t = new StringTokenizer(cd,"|");
+        t.nextToken();
+        t.nextToken();
+        t.nextToken();
+        String precioString = t.nextToken();
+        precioString = precioString.replace('$',' ').trim();
+
+        float precio=Float.parseFloat(precioString);
+        float cantidad=this.cds.get(cd);
+
+        return precio*cantidad;
+    }
+
+    //Devuelve el importe total
+    public float getImporteTotal(){
+
+        float importe=0;
+        for(String cd : this.cds.keySet()){
+            StringTokenizer t = new StringTokenizer(cd,"|");
+            t.nextToken();
+            t.nextToken();
+            t.nextToken();
+            String precioString = t.nextToken();
+            precioString = precioString.replace('$',' ').trim();
+
+            float precio=Float.parseFloat(precioString);
+            float cantidad=this.cds.get(cd);
+            importe+=precio*cantidad;
+        }
+
+        return importe;
+    }
+
+    //Metodo privado para debuguear
+    private void imprimirTodo(){
         System.out.println("Contenido");
         for(String cd : cds.keySet()){
             System.out.printf("\t%s, %d\n",cd,cds.get(cd));
--- WEB-INF/classes/Seleccionar.java
+++ WEB-INF/classes/Seleccionar.java
@@ -7,10 +7,9 @@ public class Seleccionar extends HttpServlet{
         
         response.setContentType("text/html");
 
-        //Hay que meter codigo de sesion
+        //Se obtienen la sesion y el carrito
         HttpSession sesion=request.getSession();
         Carrito carrito=null;
-        System.out.println("test");
 
         if(sesion.isNew()){
             carrito= new Carrito();
@@ -23,7 +22,51 @@ public class Seleccionar extends HttpServlet{
         carrito.anadirCD(request.getParameter("titulo"),
             Integer.parseInt(request.getParameter("cantidad")));
 
-        //Hay que meter codigo de siguiente pagina
+        //Se devuelve la pagina de visualizacion del carrito
+        //!!Ahora solo devuelve la pagina original
+        PrintWriter out=response.getWriter();
+        out.println("<html>" +
+            "<head>" +
+            "<title>Musica para DAA</title>" +
+            "<meta charset=\"UTF-8\">" +
+            "</head>" +
+            "<body bgcolor=\"#FDF5E6\">" +
+            "<table align=\"center\" border=\"0\">" +
+            "<tr>" +
+            "<th><IMG SRC=\"\" ALIGN=\"CENTER\"></th>" +
+            "<th><font face=\"Times New Roman,Times\" size=\"+3\">Música para DAA</font></th>" +
+            "<th><IMG SRC=\"\" ALIGN=\"CENTER\"></th>" +
+            "</tr>" +
+            "</table>" +
+            "<hr>" +
+            "<p>" +
+            "<center>" +
+            "<form action=\"seleccionar\">" +
+            "<b>CD:</b>" +
+            "<select name=\"titulo\">" +
+            "<option>Yuan | The Guo Brothers | China | $14.95</option>" +
+            "<option>Drums of Passion | Babatunde Olatunji | Nigeria | $16.95</option>" +
+            "<option>Kaira | Tounami Diabate| Mali | $16.95</option>" +
+            "<option>The Lion is Loose | Eliades Ochoa | Cuba | $13.95</option>" +
+            "<option>Dance the Devil Away | Outback | Australia | $14.95</option>" +
+            "<option>Record of Changes | Samulnori | Korea | $12.95</option>" +
+            "<option>Djelika | Tounami Diabate | Mali | $14.95</option>" +
+            "<option>Rapture | Nusrat Fateh Ali Khan | Pakistan | $12.95</option>" +
+            "<option>Cesaria Evora | Cesaria Evora | Cape Verde | $16.95</option>" +
+            "<option>DAA | GSTIC | Spain | $50.00</option>" +
+            "</select>" +
+            "<b>Cantidad:</b>" +
+            "<input type=\"text\" name=\"cantidad\" value=\"1\">" +
+            "<p>" +
+            "<center>" +
+            "<input type=\"submit\" value=\"Selecciona Producto\">" +
+            "</center>" +
+            "</form>" +
+            "</center>" +
+            "<hr>" +
+            "</body>" +
+            "</html>"
+        );
     }
 
 }
